17ecc86315a6cfabb5204ff8f4889456ec1a3106
Maduru Sadak Pramodh <sadakpramodh@outlook.com>
2023-08-26 00:23:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 # Second
-import streamlit as st import anthropic
+import streamlit as st
+import anthropic
 with st.sidebar:
     anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
